c5b6087a72ed87ec570c4b64f695e5e8a184610b
Atharva Patil <atharvapatilpune@gmail.com>
2025-01-05 02:06:17 +0530

    Flatten `test`.

--- apps/cli/src/commands/test.ts
+++ apps/cli/src/commands/test.ts
@@ -1,23 +1,35 @@
 import yargs from 'yargs';
-import { testStack } from '../../actions/test';
-import { SCOPE } from '../../lib/engine/scope_spec';
-import { graphite } from '../../lib/runner';
+import { testStack } from '../actions/test';
+import { SCOPE, TScopeSpec } from '../lib/engine/scope_spec';
+import { graphite } from '../lib/runner';
 
 const args = {
   command: {
     describe: `The command you'd like to run on each branch of your stack.`,
-    demandOption: true,
     type: 'string',
+    demandOption: true,
     alias: 'c',
     positional: true,
     hidden: true,
   },
   trunk: {
     describe: `Run the command on the trunk branch in addition to the rest of the stack.`,
+    type: 'boolean',
     demandOption: false,
     default: false,
     alias: 't',
+  },
+  downstack: {
+    describe: `Run the command on each branch from the trunk to the current branch.`,
     type: 'boolean',
+    demandOption: false,
+    alias: 'd',
+  },
+  upstack: {
+    describe: `Run the command for each of the current branch and its descendants.`,
+    type: 'boolean',
+    demandOption: false,
+    alias: 'u',
   },
 } as const;
 type argsT = yargs.Arguments<yargs.InferredOptionTypes<typeof args>>;
@@ -29,9 +41,18 @@ export const description =
   'Run the provided command on each branch in the current stack and aggregate the results.';
 export const builder = args;
 export const handler = async (argv: argsT): Promise<void> =>
-  graphite(argv, canonical, async (context) =>
+  graphite(argv, canonical, async (context) => {
+    let scope: TScopeSpec;
+    if (argv.downstack) {
+      scope = SCOPE.DOWNSTACK;
+    } else if (argv.upstack) {
+      scope = SCOPE.UPSTACK;
+    } else {
+      scope = SCOPE.STACK;
+    }
+
     testStack(
-      { scope: SCOPE.STACK, includeTrunk: argv.trunk, command: argv.command },
+      { scope, includeTrunk: argv.trunk, command: argv.command },
       context
-    )
-  );
+    );
+  });
